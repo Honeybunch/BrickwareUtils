91e2288cdf51850faafdcb9a92a62a3e6dc70070
Honeybunch <amt3824@g.rit.edu>
2015-02-06 19:47:28 -0500

    DirectX rendering works now. It even has input and spawning but the shader and normals are a bit wonky
    Former-commit-id: c71a812672b03d2f0eb122516da76c56109adbd2 [formerly cecdecb919a350771d8f19636d50805afa3ec4a5 [formerly ccd75fa1d49e3a20c1a9a54574a4576413e19cdb]]
    Former-commit-id: 5971fb80475fd5199fe9a5e162294b89779f0db2
    Former-commit-id: ce4796b14b0409338a8c563248fe42385e3f2dc6

---
+++ Debug/Shaders/PixelShader.hlsl
@@ -0,0 +1,37 @@
+
+// Defines the input to this pixel shader
+// - Should match the output of our corresponding vertex shader
+struct VertexToPixel
+{
+	float4 position		: SV_POSITION;
+	float3 L : LIGHT_L;
+	float3 E : LIGHT_E;
+	float3 H : LIGHT_H;
+	float3 N : LIGHT_N;
+};
+
+// Entry point for this pixel shader
+float4 main(VertexToPixel input) : SV_TARGET
+{
+	float3 ambientProduct = float3(.1, .1, .1);
+	float3 diffuseProduct = float3(.8, .8, .8);
+	float3 specularProduct = float3(.1, .1 , .1);
+
+	float gloss = 75.0;
+
+	//Color is just green for now
+	float4 hue = float4(0.0f, 0.5f, 0.0f, 1.0f);
+
+	float Kd = max(dot(input.L, input.N), 0.0);
+	float Ks = pow(max(dot(input.N, input.H), 0.0), gloss);
+
+	float3 ambient = hue.rgb * ambientProduct;
+	float3 diffuse = hue.rgb * Kd * diffuseProduct;
+	float3 specular = float3(1, 1, 1) * Ks * specularProduct;
+
+	if (dot(input.L, input.N) < 0.0)
+		specular = float3(0.0, 0.0, 0.0);
+
+	float4 color = float4((ambient + diffuse + specular), hue.a);
+	return color;
+}
---
+++ Debug/Shaders/VertexShader.hlsl
@@ -0,0 +1,78 @@
+//Pack matrices so that they're all row major like GLSL
+#pragma pack_matrix(row_major)
+
+// The constant buffer that holds our "per model" data
+// - Each object you draw with this shader will probably have
+//   slightly different data (at least for the world matrix)
+cbuffer perModel : register(b0)
+{
+	matrix modelMatrix;
+	matrix viewMatrix;
+	matrix projectionMatrix;
+
+	float3 lightPosition;
+
+	texture2D tex;
+
+	float3 lookAt;
+	float3 eyePoint;
+};
+
+// Defines what kind of data to expect as input
+// - This should match our input layout!
+struct VertexShaderInput
+{
+	float3 position		: POSITION;
+	float3 normal		: NORMAL;
+	float2 texCoord		: TEXCOORD;
+};
+
+// Defines the output data of our vertex shader
+// - At a minimum, you'll need an SV_POSITION
+// - Should match the pixel shader's input
+struct VertexToPixel
+{
+	float4 position		: SV_POSITION; // System Value Position - Has specific meaning to the pipeline!
+	float3 L : LIGHT_L;
+	float3 E : LIGHT_E;
+	float3 H : LIGHT_H;
+	float3 N : LIGHT_N;
+};
+
+// The entry point for our vertex shader
+VertexToPixel main(VertexShaderInput input)
+{
+	// Set up output
+	VertexToPixel output;
+
+	// Calculate output position
+	matrix modelViewProj = mul(mul(modelMatrix, viewMatrix), projectionMatrix);
+	output.position = mul(float4(input.position, 1.0f), modelViewProj);
+
+	//try to do some lighting
+	float4 worldCoord4v = mul(modelMatrix, float4(input.position, 1.0f));
+	float3 worldCoord3v = float3(worldCoord4v.x / worldCoord4v.w,
+								 worldCoord4v.y / worldCoord4v.w,
+								 worldCoord4v.z / worldCoord4v.w);
+
+	matrix rotation = matrix(modelMatrix[0][0], modelMatrix[1][0], modelMatrix[2][0], 0,
+							 modelMatrix[0][1], modelMatrix[1][1], modelMatrix[2][1], 0,
+							 modelMatrix[0][2], modelMatrix[1][2], modelMatrix[2][2], 0,
+							 0, 0, 0, 1);
+	float4 worldNormal4v = mul(rotation,  float4(input.normal, 1.0));
+	float3 worldNormal3v = float3(worldNormal4v.x / worldNormal4v.w,
+								  worldNormal4v.y / worldNormal4v.w,
+								  worldNormal4v.z / worldNormal4v.w);
+
+	float3 L = normalize(lightPosition - worldCoord3v);
+	float3 E = normalize(eyePoint - worldCoord3v);
+	float3 H = normalize(L + E);
+	float3 N = normalize(worldNormal3v);
+
+	output.L = L;
+	output.E = E;
+	output.H = H;
+	output.N = N;
+
+	return output;
+}
